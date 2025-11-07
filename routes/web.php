667d373bf0ef0c6370dcb0e676aef5f9df977e46
LastCodeTech <sulaimonabubakre260@gmail.com>
2025-11-07 23:34:20 +0100

    fjfjf

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
-// Route::get('/about', function(){
-//     return view('about');
-// });
-
-Route::get('/contact-me', function(){
-    return view('contactus');
-})->name('ct');
+// Route::get('/contact-me', function(){
+//     return view('contactus');
+// })->name('ct');
 
 
 
